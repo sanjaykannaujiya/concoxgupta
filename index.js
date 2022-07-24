8d064dd208461dd2ca2a5fb3156687dd9caee703
Sanjay <sballia42@gmail.com>
2022-07-24 16:47:40 +0530

    add new file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const date_and_time=require('date-and-time')
 const app = express()
 const mongoose = require("mongoose")
 const bodyparser = require("body-parser");
-const port = 4000;
+const port = process.env.port || 3000;
 app.use(express.json())
 app.use(bodyparser.json())
 const Db = require("./Inventri/config/db")
